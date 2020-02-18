c47a4b377e71c2afc4dd0d08ff0be388f193621e
Tang Xiaohu <v-xiat@microsoft.com>
2020-02-18 22:48:25 +0800

    Add get_tflite_model_details

--- main.py
+++ main.py
@@ -11,21 +11,24 @@ from accuracy_tester.data_preparers.data_preparer_def import DataPreparerDef
 
 from utils.connection import Adb, Ssh, Connection
 
+from preprocess.model_archive import get_tflite_model_details
+
 
 def model_latency_test():
     from testers.tester_impls.test_model import TestModel
     from testers.inference_sdks.tflite import Tflite
-    from testers.inference_sdks.rknn import Rknn
+    # from testers.inference_sdks.rknn import Rknn
     from testers.sampling.model_sampler import ModelSampler
 
     tester = TestModel(settings={
-        "connection": Adb("5e6fecf", False),
-        "inference_sdk": Rknn({
-            "rknn_target": "rk1808",
-            "input_imsize": 224
+        "connection": Adb("2e98c8a5", False),
+        "inference_sdk": Tflite({
+            "benchmark_model_path": "/data/local/tmp/tf-r2.1-60afa4e/benchmark_model",
         }),
         "sampler": ModelSampler({
-            "model_paths": glob("C:/Users/v-xiat/Microsoft/Shihao Han (FA Talent) - ChannelNas/models/rknn/inception_v4/*.rknn")
+            "model_paths":
+            list(map(lambda x: x.model_path,
+                     get_tflite_model_details("float16_quant")))
         })
     })
 
@@ -39,12 +42,11 @@ def model_latency_test():
 def accuracy_test_tflite():
     from accuracy_tester.accuracy_tester import AccuracyTester
     from accuracy_tester.accuracy_evaluators.tflite import Tflite
-    from preprocess.model_archive import tflite_model_details
 
     tester = AccuracyTester({
-        "zip_size": 5,
+        "zip_size": 50000,
         "dataset_size": 10,
-        "model_details": tflite_model_details,
+        "model_details": get_tflite_model_details(),
         "data_preparer": DataPreparerDef({
             "labels_path": "C:/Users/tigertang/Projects/dataset/val_labels.txt",
             "validation_set_path": "C:/Users/tigertang/Projects/dataset/validation",
@@ -120,4 +122,4 @@ def layer_latency_test_rknn():
 
 
 if __name__ == '__main__':
-    accuracy_test_tflite()
+    model_latency_test()
--- preprocess/model_archive.py
+++ preprocess/model_archive.py
@@ -1,5 +1,6 @@
 import os
 from collections import namedtuple
+from typing import List
 
 from .factory import *
 
@@ -11,69 +12,97 @@ _onedrive_path = os.path.expanduser(
     "~/Microsoft/Shihao Han (FA Talent) - ChannelNas/models")
 
 
-tflite_model_details = [
-    ModelDetail(
-        _onedrive_path + "/tflite/shufflenet_v1/shufflenet_v1_g3_1.5.tflite",
-        shufflenet_preprocess, "input.1", "535"
-    ),
-    ModelDetail(
-        _onedrive_path + "/tflite/shufflenet_v1/shufflenet_v1_g8_1.0.tflite",
-        shufflenet_preprocess, "input.1", "535"
-    ),
-    ModelDetail(
-        _onedrive_path + "/tflite/shufflenet_v2/shufflenet_v2_1.0.tflite",
-        shufflenet_preprocess, "input.1", "626"
-    ),
-    ModelDetail(
-        _onedrive_path + "/tflite/shufflenet_v2/shufflenet_v2_1.5.tflite",
-        shufflenet_preprocess, "input.1", "626"
-    ),
-    ModelDetail(
-        _onedrive_path + "/tflite/mobilenet_v1/mobilenet_v1_1.0_224.tflite",
-        inception_224_preprocess, "input", "MobilenetV1/Predictions/Reshape_1"
-    ),
-    ModelDetail(
-        _onedrive_path + "/tflite/mobilenet_v2/mobilenet_v2_1.0_224.tflite",
-        inception_224_preprocess, "input", "MobilenetV2/Predictions/Reshape_1"
-    ),
-    ModelDetail(
-        _onedrive_path + "/tflite/mobilenet_v3/mobilenet_v3_large_224_1.0.tflite",
-        inception_224_preprocess, "input", "MobilenetV3/Predictions/Softmax"
-    ),
-    ModelDetail(
-        _onedrive_path + "/tflite/inception_v1/inception_v1.tflite",
-        inception_224_preprocess, "input", "InceptionV1/Logits/Predictions/Reshape_1"
-    ),
-    ModelDetail(
-        _onedrive_path + "/tflite/inception_v4/inception_v4.tflite",
-        inception_299_preprocess, "input", "InceptionV4/Logits/Predictions"
-    ),
-    ModelDetail(
-        _onedrive_path + "/tflite/mnasnet/mnasnet_a1.tflite",
-        mnasnet_preprocess, "Placeholder", "logits"
-    ),
-    ModelDetail(
-        _onedrive_path + "/tflite/nasnet/nasnet_a_mobile.tflite",
-        inception_224_preprocess, "input", "final_layer/predictions"
-    ),
-    ModelDetail(
-        _onedrive_path + "/tflite/proxyless/proxyless_mobile.tflite",
-        proxyless_preprocess, "input_images", "classifier/linear/add"
-    ),
-    ModelDetail(
-        _onedrive_path + "/tflite/proxyless/proxyless_mobile_14.tflite",
-        proxyless_preprocess, "input_images", "classifier/linear/add"
-    ),
-    ModelDetail(
-        _onedrive_path + "/tflite/resnet_v2/resnet_v2_50_299.tflite",
-        inception_299_preprocess, "input", "resnet_v2_50/predictions/Reshape_1"
-    ),
-    ModelDetail(
-        _onedrive_path + "/tflite/efficientnet/efficientnet_b0.tflite",
-        efficientnet_b0_preprocess, "images", "Softmax"
-    ),
-    ModelDetail(
-        _onedrive_path + "/tflite/efficientnet/efficientnet_b1.tflite",
-        efficientnet_b1_preprocess, "images", "Softmax"
-    )
-]
+def get_tflite_model_details(subdiv=None) -> List[ModelDetail]:
+    f32_model_details = [
+        ModelDetail(
+            _onedrive_path + "/tflite/shufflenet_v1/shufflenet_v1_g3_1.5.tflite",
+            shufflenet_preprocess, "input.1", "535"
+        ),
+        ModelDetail(
+            _onedrive_path + "/tflite/shufflenet_v1/shufflenet_v1_g8_1.0.tflite",
+            shufflenet_preprocess, "input.1", "535"
+        ),
+        ModelDetail(
+            _onedrive_path + "/tflite/shufflenet_v2/shufflenet_v2_1.0.tflite",
+            shufflenet_preprocess, "input.1", "626"
+        ),
+        ModelDetail(
+            _onedrive_path + "/tflite/shufflenet_v2/shufflenet_v2_1.5.tflite",
+            shufflenet_preprocess, "input.1", "626"
+        ),
+        ModelDetail(
+            _onedrive_path + "/tflite/mobilenet_v1/mobilenet_v1_1.0_224.tflite",
+            inception_224_preprocess, "input", "MobilenetV1/Predictions/Reshape_1"
+        ),
+        ModelDetail(
+            _onedrive_path + "/tflite/mobilenet_v2/mobilenet_v2_1.0_224.tflite",
+            inception_224_preprocess, "input", "MobilenetV2/Predictions/Reshape_1"
+        ),
+        ModelDetail(
+            _onedrive_path + "/tflite/mobilenet_v3/mobilenet_v3_large_224_1.0.tflite",
+            inception_224_preprocess, "input", "MobilenetV3/Predictions/Softmax"
+        ),
+        ModelDetail(
+            _onedrive_path + "/tflite/inception_v1/inception_v1.tflite",
+            inception_224_preprocess, "input", "InceptionV1/Logits/Predictions/Reshape_1"
+        ),
+        ModelDetail(
+            _onedrive_path + "/tflite/inception_v4/inception_v4.tflite",
+            inception_299_preprocess, "input", "InceptionV4/Logits/Predictions"
+        ),
+        ModelDetail(
+            _onedrive_path + "/tflite/mnasnet/mnasnet_a1.tflite",
+            mnasnet_preprocess, "Placeholder", "logits"
+        ),
+        ModelDetail(
+            _onedrive_path + "/tflite/nasnet/nasnet_a_mobile.tflite",
+            inception_224_preprocess, "input", "final_layer/predictions"
+        ),
+        ModelDetail(
+            _onedrive_path + "/tflite/proxyless/proxyless_mobile.tflite",
+            proxyless_preprocess, "input_images", "classifier/linear/add"
+        ),
+        ModelDetail(
+            _onedrive_path + "/tflite/proxyless/proxyless_mobile_14.tflite",
+            proxyless_preprocess, "input_images", "classifier/linear/add"
+        ),
+        ModelDetail(
+            _onedrive_path + "/tflite/resnet_v2/resnet_v2_50_299.tflite",
+            inception_299_preprocess, "input", "resnet_v2_50/predictions/Reshape_1"
+        ),
+        ModelDetail(
+            _onedrive_path + "/tflite/efficientnet/efficientnet_b0.tflite",
+            efficientnet_b0_preprocess, "images", "Softmax"
+        ),
+        ModelDetail(
+            _onedrive_path + "/tflite/efficientnet/efficientnet_b1.tflite",
+            efficientnet_b1_preprocess, "images", "Softmax"
+        )
+    ]
+    all_model_details = []
+    for model_detail in f32_model_details:
+        for suffix in ["", "_float16_quant", "_weight_quant", "int8_quant"]:
+            all_model_details.append(ModelDetail(
+                model_path=model_detail.model_path.replace(
+                    ".tflite", suffix + ".tflite"),
+                preprocess=model_detail.preprocess,
+                input_node=model_detail.input_node,
+                output_node=model_detail.output_node
+            ))
+
+    if subdiv is None:
+        def filter_func(_): return True
+    elif isinstance(subdiv, str):
+        def filter_func(model_detail): return subdiv in model_detail.model_path
+    elif isinstance(subdiv, List[str]):
+        def filter_func(model_detail: ModelDetail):
+            for i in subdiv:
+                if i in model_detail.model_path:
+                    return True
+            return False
+    elif isinstance(subdiv, types.FunctionType):
+        filter_func = subdiv
+    else:
+        assert False
+
+    return list(filter(filter_func, all_model_details))
