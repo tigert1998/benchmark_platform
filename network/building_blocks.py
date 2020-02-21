574d378c8c2117561144b4b0adf66c9f76081c11
Tang Xiaohu <v-xiat@microsoft.com>
2020-02-21 15:24:44 +0800

    Add test_shufflenet_v1_unit

--- network/building_blocks.py
+++ network/building_blocks.py
@@ -1,6 +1,17 @@
 import tensorflow as tf
 
 
+def channel_shuffle(features, num_groups: int):
+    _, h, w, c = features.get_shape().as_list()
+    assert c % num_groups == 0
+
+    with tf.variable_scope('channel_shuffle'):
+        reshaped = tf.reshape(
+            features, [-1, w, num_groups, c // num_groups])
+        transposed = tf.transpose(reshaped, [0, 1, 3, 2])
+        return tf.reshape(transposed, [-1, h, w, c])
+
+
 def grouped_conv(features, num_groups: int, stride: int, kernel_size: int, num_outputs: int):
     cin = features.get_shape().as_list()[-1]
     cout = num_outputs
@@ -21,20 +32,29 @@ def grouped_conv(features, num_groups: int, stride: int, kernel_size: int, num_o
     return net
 
 
-def mbnet_v1_block(features, stride: int, kernel_size: int, num_outputs: int):
+def depthwise_conv(features, stride: int, kernel_size: int):
     cin = features.get_shape().as_list()[-1]
-
-    with tf.variable_scope("mbnet_v1_block"):
-        net = tf.nn.relu6(tf.nn.depthwise_conv2d(
+    with tf.variable_scope("depthwise_conv"):
+        return tf.nn.depthwise_conv2d(
             features,
             filter=tf.get_variable(
-                "weight", [kernel_size, kernel_size, cin, 1],
+                "kernel",
+                [kernel_size, kernel_size, cin, 1],
                 dtype=features.dtype, trainable=True
             ),
             strides=[1, stride, stride, 1],
             padding='SAME',
             rate=[1, 1],
-        ))
+        )
+
+
+def mbnet_v1_block(features, stride: int, kernel_size: int, num_outputs: int):
+    cin = features.get_shape().as_list()[-1]
+
+    assert stride in [1, 2]
+
+    with tf.variable_scope("mbnet_v1_block"):
+        net = tf.nn.relu6(depthwise_conv(features, stride, kernel_size))
         net = tf.nn.relu6(tf.keras.layers.Conv2D(
             filters=num_outputs,
             kernel_size=[1, 1],
@@ -48,6 +68,7 @@ def mbnet_v2_block(features, expansion_rate: int,
                    stride: int, kernel_size: int, num_outputs: int):
     cin = features.get_shape().as_list()[-1]
     cout = num_outputs
+    assert stride in [1, 2]
     if stride == 1:
         assert cin == cout
 
@@ -59,18 +80,7 @@ def mbnet_v2_block(features, expansion_rate: int,
                 strides=[1, 1],
                 padding='same')(features))
 
-        with tf.variable_scope("depthwise_conv"):
-            net = tf.nn.relu6(tf.nn.depthwise_conv2d(
-                net,
-                filter=tf.get_variable(
-                    "kernel",
-                    [kernel_size, kernel_size, cin * expansion_rate, 1],
-                    dtype=features.dtype, trainable=True
-                ),
-                strides=[1, stride, stride, 1],
-                padding='SAME',
-                rate=[1, 1],
-            ))
+        net = tf.nn.relu6(depthwise_conv(features, stride, kernel_size))
 
         with tf.variable_scope("second_conv"):
             net = tf.keras.layers.Conv2D(
@@ -84,3 +94,44 @@ def mbnet_v2_block(features, expansion_rate: int,
             net = features + net
 
     return net
+
+
+def batch_normalization(features):
+    # prevent bias from removal
+    return tf.layers.batch_normalization(
+        features,
+        beta_initializer=tf.constant_initializer(1e-3)
+    )
+
+
+def shufflenet_v1_unit(features, num_groups: int, mid_channels: int,
+                       stride: int, kernel_size: int, num_outputs: int):
+    assert stride in [1, 2]
+
+    cin = features.get_shape().as_list()[-1]
+
+    if stride == 1:
+        assert num_outputs == cin
+        major_branch_num_outputs = num_outputs
+    else:
+        assert num_outputs > cin
+        major_branch_num_outputs = num_outputs - cin
+
+    with tf.variable_scope("shufflenet_v1_unit"):
+        net = grouped_conv(features, num_groups, 1, 1, mid_channels)
+        net = tf.nn.relu(batch_normalization(net))
+        net = channel_shuffle(net, num_groups)
+        net = depthwise_conv(net, stride, kernel_size)
+        net = batch_normalization(net)
+        net = grouped_conv(net, num_groups, 1, 1, major_branch_num_outputs)
+        net = batch_normalization(net)
+
+        if stride == 1:
+            net = net + features
+        else:
+            minor_branch = tf.keras.layers.AveragePooling2D(
+                pool_size=(3, 3), strides=2, padding='same')(features)
+            net = tf.concat([minor_branch, net], axis=-1)
+
+        net = tf.nn.relu(net)
+        return net
--- testers/tester_impls/test_shufflenet_v1_unit.py
+++ testers/tester_impls/test_shufflenet_v1_unit.py
@@ -2,13 +2,13 @@ from testers.tester import Tester
 
 import tensorflow as tf
 
-from network.building_blocks import mbnet_v2_block
+from network.building_blocks import shufflenet_v1_unit
 
 
-class TestMbnetV2Block(Tester):
+class TestShufflenetV1Unit(Tester):
     def _generate_model(self, sample):
         model_path = "model"
-        _, input_imsize, cin, cout, stride, kernel_size = sample
+        _, input_imsize, cin, cout, num_groups, mid_channels, stride, kernel_size = sample
 
         tf.reset_default_graph()
 
@@ -16,7 +16,8 @@ class TestMbnetV2Block(Tester):
             name="input_im", dtype=tf.float32,
             shape=(1, input_imsize, input_imsize, cin))
 
-        net = mbnet_v2_block(input_im, 6, stride, kernel_size, cout)
+        net = shufflenet_v1_unit(
+            input_im, num_groups, mid_channels, stride, kernel_size, cout)
 
         self.inference_sdk.generate_model(model_path, [input_im], [net])
         return model_path, input_im.get_shape().as_list()
