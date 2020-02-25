7593b3a1c57f902d3800494270a67dc52a81b91f
Tang Xiaohu <v-xiat@microsoft.com>
2020-02-25 21:17:33 +0800

    Move inference_sdk to inner iterations

--- bin/op_block_bench_suite.py
+++ bin/op_block_bench_suite.py
@@ -1,3 +1,5 @@
+import itertools
+
 import tensorflow as tf
 
 from testers.inference_sdks.tflite import Tflite
@@ -56,32 +58,38 @@ def tflite_cpu_main():
 
     connection = Adb("2e98c8a5", False)
 
-    for inference_sdk in inference_sdks:
-        for tester_class, sampler_class, name in [
-            (TestConv, OpExperimentConvSampler, "conv"),
-            (TestDwconv, OpExperimentDwconvSampler, "dwconv"),
-            (TestDilatedConv, DilatedConvSampler, "dilated_conv"),
-            (TestGconv, GconvSampler, "grouped_conv"),
-            (TestAdd, AddSampler, "add"),
-            (TestConcat, ConcatSampler, "concat"),
-            (TestGlobalPooling, GlobalPoolingSampler, "global_pooling"),
-            (TestFc, OpExperimentFcSampler, "fc"),
-            (TestShuffle, ShuffleSampler, "shuffle"),
+    for tester_class, sampler_class, name in [
+        (TestConv, OpExperimentConvSampler, "conv"),
+        (TestDwconv, OpExperimentDwconvSampler, "dwconv"),
+        (TestDilatedConv, DilatedConvSampler, "dilated_conv"),
+        (TestGconv, GconvSampler, "grouped_conv"),
+        (TestAdd, AddSampler, "add"),
+        (TestConcat, ConcatSampler, "concat"),
+        (TestGlobalPooling, GlobalPoolingSampler, "global_pooling"),
+        (TestFc, OpExperimentFcSampler, "fc"),
+        (TestShuffle, ShuffleSampler, "shuffle"),
 
-            (TestMbnetV1Block, MbnetV1BlockSampler, "mbnet_v1_block"),
-            (TestMbnetV2Block, MbnetV2BlockSampler, "mbnet_v2_block"),
-            (TestShufflenetV1Unit, ShufflenetV1UnitSampler, "shufflenet_v1_unit"),
-            (TestShufflenetV2Unit, ShufflenetV2UnitSampler, "shufflenet_v2_unit"),
-            (TestResnetV1Block, ResnetV1BlockSampler, "resnet_v1_block"),
-            (TestDenseBlock, DenseBlockSampler, "dense_block"),
+        (TestMbnetV1Block, MbnetV1BlockSampler, "mbnet_v1_block"),
+        (TestMbnetV2Block, MbnetV2BlockSampler, "mbnet_v2_block"),
+        (TestShufflenetV1Unit, ShufflenetV1UnitSampler, "shufflenet_v1_unit"),
+        (TestShufflenetV2Unit, ShufflenetV2UnitSampler, "shufflenet_v2_unit"),
+        (TestResnetV1Block, ResnetV1BlockSampler, "resnet_v1_block"),
+        (TestDenseBlock, DenseBlockSampler, "dense_block"),
 
-            (TestMixConv, MixConvSampler, "mix_conv")
-        ]:
+        (TestMixConv, MixConvSampler, "mix_conv")
+    ]:
+        for inference_sdk in inference_sdks:
+            if inference_sdk.settings["quantization"] == "":
+                subdir = "none"
+            else:
+                subdir = inference_sdk.settings["quantization"]
+            subdir = "{}/{}".format(name, subdir)
+
             concrete_tester = tester_class({
                 "connection": connection,
                 "inference_sdk": inference_sdk,
                 "sampler": sampler_class(),
-                "subdir": name,
+                "subdir": subdir,
                 "resume_from": None
             })
             concrete_tester.run({
