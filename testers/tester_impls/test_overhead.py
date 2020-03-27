7ee41116e40ee4108c80f3b84e880fe7ead38002
Tang Xiaohu <v-xiat@microsoft.com>
2020-03-27 17:26:15 +0800

    Add identity to test_overhead

--- testers/tester_impls/test_overhead.py
+++ testers/tester_impls/test_overhead.py
@@ -33,6 +33,10 @@ class TestOverhead(Tester):
         )
 
     @staticmethod
+    def _id_add_layer(net: tf.Tensor) -> tf.Tensor:
+        return tf.identity(net)
+
+    @staticmethod
     def default_settings():
         return {
             **Tester.default_settings(),
@@ -45,10 +49,12 @@ class TestOverhead(Tester):
         super().__init__(settings)
         dic = {
             "conv": self._conv_add_layer,
-            "dwconv": self._dwconv_add_layer
+            "dwconv": self._dwconv_add_layer,
+            "id": self._id_add_layer
         }
         assert self.settings["op"] in dic
         self.add_layer = dic[self.settings["op"]]
+        self.is_id = self.settings["op"] == "id"
 
     def fetch_overall_latency(self, imsize: int, cin: int, n: int) -> (float, float):
         """Fetch overall latency of a stacked model
@@ -78,27 +84,39 @@ class TestOverhead(Tester):
 
     def _process_inference_result(self, result: InferenceResult):
         ret = {}
-        for i in [self.settings["min"], self.settings["max"]]:
-            ret["{}_avg_ms".format(i)] = result.profiling_details[i][0]
-            ret["{}_std_ms".format(i)] = result.profiling_details[i][1]
+        for i, (avg_ms, std_ms) in result.profiling_details["results"].items():
+            ret["{}_avg_ms".format(i)] = avg_ms
+            ret["{}_std_ms".format(i)] = std_ms
         ret["estimated"] = result.profiling_details["estimated"]
         return ret
 
     def _test_sample(self, sample):
         imsize, cin = sample
 
-        min_res = self.fetch_overall_latency(imsize, cin, self.settings["min"])
-        max_res = self.fetch_overall_latency(imsize, cin, self.settings["max"])
-        layer_lat = (max_res[0] - min_res[0]) / \
-            (self.settings["max"] - self.settings["min"])
-        estimated_overhead = (
-            max_res[0] - layer_lat * self.settings["max"]) / 2
+        if self.is_id:
+            todos = [1]
+        else:
+            todos = [self.settings["min"], self.settings["max"]]
+
+        results = {
+            i: self.fetch_overall_latency(imsize, cin, i)
+            for i in todos
+        }
+
+        if self.is_id:
+            estimated_overhead = results[1].avg_ms / 2
+        else:
+            max_res = results[self.settings["max"]]
+            min_res = results[self.settings["min"]]
+            layer_latency = (max_res[0] - min_res[0]) / \
+                (self.settings["max"] - self.settings["min"])
+            estimated_overhead = (
+                max_res[0] - layer_latency * self.settings["max"]) / 2
 
         return InferenceResult(
             avg_ms=None, std_ms=None,
             profiling_details={
-                self.settings["min"]: min_res,
-                self.settings["max"]: max_res,
+                "results": results,
                 "estimated": estimated_overhead
             },
             layerwise_info=None
