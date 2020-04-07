ebbc7364656b46eb1eddf4b0f050fd255f296b00
MarsTechHAN <hansh-sz@hotmail.com>
2020-04-08 02:27:48 +0800

    Change add to tf.math.add for snpe to eval

--- network/building_ops.py
+++ network/building_ops.py
@@ -72,7 +72,7 @@ def squeeze_and_excitation(features, mid_channels: int):
     """SE layer
     https://github.com/tensorflow/models/blob/89dd9a4e2548e8a5214bd4e564428d01c206a7db/research/slim/nets/mobilenet/conv_blocks.py#L408
     """
-    def gating_fn(features): return tf.nn.relu6(features + 3) * 0.16667
+    def gating_fn(features): return tf.nn.relu6( tf.math.add(features, 3)) * 0.16667
 
     with tf.variable_scope("squeeze_and_excitation"):
         net = global_pooling(features)
--- network/shufflenet_units.py
+++ network/shufflenet_units.py
@@ -17,21 +17,21 @@ def shufflenet_v1_unit(features, num_groups: int, mid_channels: int,
         assert num_outputs > cin
         major_branch_num_outputs = num_outputs - cin
 
-    with tf.variable_scope("shufflenet_v1_unit"):
+    with tf.variable_scope("shufflenet_v1_unit"):   
         net = grouped_conv(features, num_groups, 1, 1, mid_channels)
         net = tf.nn.relu(batch_normalization(net))
         net = channel_shuffle(net, num_groups)
-        net = depthwise_conv(net, stride, kernel_size)
+        net = depthwise_conv(net, stride, kernel_size) 
         net = batch_normalization(net)
         net = grouped_conv(net, num_groups, 1, 1, major_branch_num_outputs)
         net = batch_normalization(net)
 
         if stride == 1:
-            net = net + features
+            net = tf.math.add(net, features)
         else:
-            minor_branch = tf.keras.layers.AveragePooling2D(
-                pool_size=(3, 3), strides=2, padding='same')(features)
-            net = tf.concat([minor_branch, net], axis=-1)
+           minor_branch = tf.keras.layers.AveragePooling2D(
+               pool_size=(3, 3), strides=2, padding='same')(features)
+           net = tf.concat([minor_branch, net], axis=-1)
 
         net = tf.nn.relu(net)
         return net
@@ -77,4 +77,5 @@ def shufflenet_v2_unit(features, stride: int, kernel_size: int):
 
         net = tf.concat([minor_branch, major_branch], axis=-1)
         net = channel_shuffle(net, 2)
+
         return net
